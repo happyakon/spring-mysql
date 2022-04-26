b579ce32d310b7ba7b85b340ea1f1b08f7b893f3
akon <chenbin666666@126.com>
2022-04-26 16:07:59 +0800

    新增脚本文件

--- src/main/java/com/akon/spring/mysql/SpringMysqlApplication.java
+++ src/main/java/com/akon/spring/mysql/SpringMysqlApplication.java
@@ -3,12 +3,12 @@ package com.akon.spring.mysql;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.akon.spring.mysql.model")
-
+@EnableDiscoveryClient
 public class SpringMysqlApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringMysqlApplication.class, args);
     }
